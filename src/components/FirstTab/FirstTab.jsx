eb851c4c0abaa3005af0d6df46d500c6b5773a03
jayalakshmi kv <jlkshmikv@gmail.com>
2025-05-16 11:51:29 +0530

    changes added

--- src/components/FirstTab/FirstTab.jsx
+++ src/components/FirstTab/FirstTab.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import './FirstTab.css';
+import './firstTab.css';
 
 function FirstTab() {
   const navigate = useNavigate();
